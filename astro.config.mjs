7e0bae5a8137450aa8483546ecdb2ffa6e76fc69
Aaron Ragudos <workemailaaronragudos@gmail.com>
2024-04-14 20:50:21 +0800

    fix lang value of tagalog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
-export const supportedLocales = ["en", "tl_ph"];
-export const defaultLocale = "tl_ph";
+export const supportedLocales = ["en", "tl"];
+export const defaultLocale = "tl";
 
 // https://astro.build/config
 export default defineConfig({
